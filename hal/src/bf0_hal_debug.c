51ef1e4de2f467b634bc9e11a91d4664a2035dc5
jiqunli <jiqunli@sifli.com>
2025-10-21 10:51:11 +0800

    fix bf0 hal compile link error

---
+++ hal/src/bf0_hal_debug.c
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2025 SiFli Technologies(Nanjing) Co., Ltd
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "bf0_hal.h"
+
+__weak void HAL_DBG_printf(const char *fmt, ...)
+{
+}
+
